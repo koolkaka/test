85754a500ee54b2fc9dfd70a250221c85af68066
Chu Quân <82873283+koolkaka@users.noreply.github.com>
2022-04-17 03:21:38 +0000

    something

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const config = require('./config.json');
 const fs = require('fs');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 var mess=["ngu loz", "làm đéo có thư mục đấy?","ra rửa mặt đi bạn ơi",'Mày thành hề từ khi nào thế?','Đéo biết mệt hả mày','Ôi bạn ơi! Bạn ngu là do bạn không chơi đồ đấy bạn ạ, nếu bạn chơi đồ vào thì là đề kháng nó khỏe nó không bao giờ làm mấy trò ngu đâu bạn ạ, chơi đồ là thuốc bổ mà bạn! Bạn phải nên nhớ nhá, cái viên thuốc bình thường, cái viết thuốc ACID B1 bạn mua có 2 nghìn đc mấy viên đúng k ? Hoặc là 10 nghìn 1 viên, 10 nghìn 1 viên là ACID B1 đấy , đúng không? Thế đây những viên thuốc như viên thuốc kẹo, viên thuốc kim cương, viên thuốc vương liệm này, viên thuốc các kiểu lày thì bạn mua cái đấy vào 500 nghìn 1 viên cơ mà! Chơi cái đấy vào đề kháng nó phải cao hơn chứ bạn! Chơi cái đấy vào nhiều đề kháng mà! Bạn không chơi vào đề kháng bạn kém là phải đấy bạn ạ !',"hong","Bie","dư lào bạn","Ghét","bố m nghiêp túc cười cái địt con mẹ mày à ?","bố mày nói rồi t đéo nói lại lần nữa đâu","m nghĩ đấy là trò đùa à ?","đến h chơi game r","không quen"]
+var rep="không có thư mục đấy :)"
 
 client.once('ready', () => {
     console.log('Ready!');
+    client.user.setPresence({ activities: [{ name: '/lenh đi bẹn' }], status: 'online' });
+
 });
 
 client.on('messageCreate', message => {
@@ -17,6 +20,9 @@ client.on('messageCreate', message => {
     
     const args = message.content.slice(config.prefix.length).split(/ +/);
     const command = args.shift();
+    if (message.content.toLowerCase() === config.prefix + "lenh") {
+    message.channel.send(config.prefix+"s: ảnh nude\n"+config.prefix+"sexy: no nude\n"+config.prefix+"fp: foodporn nhưng toàn salad?\n"+config.prefix+"ct: thời chiến\n"+config.prefix+"xinh: wibu's contribution\n");
+    }
     fs.readdir('./', (err, folders) => {
         var selectedFolder = null;
         folders.forEach(folder => {
@@ -42,8 +48,8 @@ client.on('messageCreate', message => {
                 message.channel.send ({files: ["./" + command.toLowerCase() + '/'+ files[imageNumber-1]]} )
             });}
       else{
-        
-        message.channel.send(test.sample(mess))
+        if(command.toLowerCase()!='lenh')
+        message.channel.send(test.sample(rep))
       }
     });
 });
